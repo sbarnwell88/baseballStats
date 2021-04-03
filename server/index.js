659621ed967f4e030ed6f6aa75de43edd53ea0c3
sbarnwell88 <susy6688@gmail.com>
2021-04-03 19:04:12 -0400

    fixed index file

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.get('/test', (req, res) => {
     res.send('Welcome to the backend!')
 })
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // app.use('/profile', playerProfile);
 app.use('/api', apiRouter)
 // app.use('/players', playersRoute);
@@ -19,11 +21,11 @@ app.use('/api', apiRouter)
 console.log(__dirname)
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join('client/build')));
+    // app.use(express.static(path.join('client/build')));
         
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
     });
 }
 
